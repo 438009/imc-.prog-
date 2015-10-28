7b2f330bd565eaf4ca021d7164d3dd779492e3a8
nick8 <nich438@hotmail.com>
2015-10-28 00:11:53 +0000

    trabaalho imc

---
+++ imc prog.c
@@ -0,0 +1,29 @@
+#include<stdio>
+int main ()
+{float P,h,IMC;
+
+printf("Digite o seu peso:\n");
+scanf("F%",&P);
+printf("Digite a sua altura:\n) 
+scanf("f%",& H);
+IMC =P/(H*H);
+
+if(imc<19);
+
+printf("o seu indice de massa corporea e %2f.e esta abaixo do peso ideal",IMC);
+
+if(IMC>20 && IMC<25);
+
+printf (" o seu inidice de massa corporea e % 2f esta no peso ideal ". IMC);
+
+if( imc>=30)
+
+printf(" o seu indice de massa corporea e %2f, esta obesa /n");
+
+return0;
+
+
+
+
+
+
